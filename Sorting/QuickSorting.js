7b31e0cf1d0792cb06ed33339757a5fe7209bcc3
ABlued <53801395+ABlued@users.noreply.github.com>
2021-11-21 00:24:58 +0900

    안정화된 퀵정렬 구현하기(기존 방식은 중복되는 수가 존재하면 작동이 불안정하였음)

--- Sorting/QuickSorting.js
+++ Sorting/QuickSorting.js
@@ -1,21 +1,39 @@
-function quickSort(array) {
-  if (array.length < 2) {
-    return array;
-  }
-  const pivot = [array[0]];
-  const left = [];
-  const right = [];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] < pivot) {
-      left.push(array[i]);
-    } else if (array[i] > pivot) {
-      right.push(array[i]);
-    } else {
-      pivot.push(array[i]);
+function divide(array, left, right, pivot) {
+  console.log(
+    `array: ${array}, left: ${array[left]}, pivot: ${pivot}, right: ${array[right]}`
+  );
+  while (left <= right) {
+    while (array[left] < pivot) {
+      left++;
     }
+    while (array[right] > pivot) {
+      right--;
+    }
+    // 서로 바꿔야할 수가 존재하는 인덱스까지 찾은 후
+    if (left <= right) {
+      let swap = array[left];
+      array[left] = array[right];
+      array[right] = swap;
+      left++;
+      right--;
+    }
+    // 바꿔야하는 인덱스를 서로 바꾼다.
   }
-  console.log(`left: ${left}, pivot: ${pivot}, right: ${right}`);
-  return quickSort(left).concat(pivot, quickSort(right));
+  return left;
 }
-const sorted = quickSort([5, 3, 8, 4, 9, 1, 6, 2, 7]);
+
+function stableQuickSort(array, left = 0, right = array.length - 1) {
+  if (left >= right) {
+    return;
+  }
+  const mid = Math.floor((left + right) / 2);
+  const pivot = array[mid];
+  const partition = divide(array, left, right, pivot);
+  stableQuickSort(array, left, partition - 1);
+  stableQuickSort(array, partition, right);
+
+  return array;
+}
+
+const sorted = stableQuickSort([5, 3, 8, 4, 5, 1, 6, 2, 7]);
 console.log(sorted);
