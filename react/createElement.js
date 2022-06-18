ef7c3c2e4523ed20d37cc334d0c1376252091528
ABlued <skwodhkswkd@naver.com>
2022-06-18 15:15:32 +0900

    refactor : 각각의 케이스들을 함수로 세분화

--- react/createElement.js
+++ react/createElement.js
@@ -1,28 +1,35 @@
 // createElement 함수 응용하기
+function createH1(props) {
+  return [document.createElement('h1')].map((element) => {
+    Object.entries({ ...props, 'data-id': 'title' }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  })[0];
+}
+function createDiv(props) {
+  return [document.createElement('div')].map((element) => {
+    Object.entries({ ...props, 'data-id': 'layout' }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  })[0];
+}
+
 function createElement(type, props) {
   switch (type) {
     case 'h1':
-      return [document.createElement('h1')].map((element) => {
-        Object.entries({ ...props, 'data-id': 'title' }).forEach(
-          ([name, value]) => element.setAttribute(name, value)
-        );
-        return element;
-      })[0];
+      return createH1(props);
     case 'div':
-      return [document.createElement('div')].map((element) => {
-        Object.entries({ ...props, 'data-id': 'layout' }).forEach(
-          ([name, value]) => element.setAttribute(name, value)
-        );
-        return element;
-      })[0];
+      return createDiv(props);
   }
 }
+
 /**
- * 이 함수 로직은 구조가 잘못되었다.
- * 그 이유는 만약 switch case h1 태그 생성 로직이 달라지면 div 생성 로직은 달라지지 않았음에도 불구하고
- * div 생성 로직도 같이 테스트를 해야 한다.
- * 특정 부분만 수정을 하였는데 그 부분만 테스트하기 어려운 구조다. 즉 변경에 용이하지 않은 구조다,
- * 이럴 경우 h1, div 를 나누고 각각 테스트할 수 있게 해줘야 한다.
+ * 아까보다는 변경에 용이한 구조이다.
+ * createH1 로직이 달라지면 createH1만 테스트하면 되기 때문이다.
+ * 하지만 이 구조의 단점이 있다. 추가에 취약한 구조이다.
+ * 이 상태에서 span 태그 추가 기능을 넣으면 createElement 코드가 수정되며 그와 상관없는 createH1과 createDiv 함수도 테스트를 해야한다.
  */
 
 document.querySelector('#root').appendChild(createElement('h1', {}));
